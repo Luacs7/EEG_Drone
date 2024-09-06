cb66dc9e15eddee68e9ef555ca0622aec7481dfc
Luacs7 <152193098+Luacs7@users.noreply.github.com>
2024-09-06 21:36:21 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+
+from src.MVC.controller_mvc import DroneController
+
+
+if __name__ == "__main__":
+    controller = DroneController()
+    controller.run()
---
+++ test.py
@@ -0,0 +1,6 @@
+from src.simulation.application import Application
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    app = Application()
+    app.show_menu()
